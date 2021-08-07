d7bf156beadaba1da18050b07f35a8c42f7ed242
Antonio Musciano <antoniomusciano@gmail.com>
2021-08-07 12:55:43 -0400

    ugly but works

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import datetime
 import random
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
+import numpy as np
 
 import tweepy
 
@@ -17,10 +17,18 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
+
+class Error(Exception):
+    pass
+
+
+class Unverified(Error):
+    pass
+
 try:
     api.verify_credentials()
     print("on")
-except:
+except Unverified:
     print("Error during authentication")
 
 
@@ -28,34 +36,36 @@ def gathering():
     plt.style.use('seaborn')
     list_of_date_times = []
     final = []
+    tweet_ids = []
 
     for status in api.user_timeline(AT):
+        # print(status.id)
+        tweet_ids.append(str(status.id))
+    for status in api.user_timeline(AT):
         list_of_date_times.append(str(status.created_at))  # time is in UTC
-
-
 
     for dates in list_of_date_times:
         times = dates.split()[-1]
         final.append(times)
 
+    print(final)
+    print(tweet_ids)
+    plotting(final, tweet_ids)
 
 
+def plotting(times, id):
+    levels = np.tile([-5, -5, -5, -5, -5, -5], int(np.ceil(len(times) / 6)))[1]
 
-def plotting(times):
-    # x =
-    # y =
+    # Create figure and plot a stem plot with the times
+    fig, ax = plt.subplots(figsize=(8.8, 5), constrained_layout=True)
+    ax.set(title="Menace Activity")
 
-    # plot
-    plt.plot([], [])
-    # plt.scatter(x, y)
+    ax.vlines(times, 0, levels, color="tab:red")  # Stems
+    ax.plot(times, np.zeros_like(times))  # Baseline
 
-    # beautify the x-labels
-    plt.gcf().autofmt_xdate()
-    myFmt = mdates.DateFormatter('%H:%M')
-    plt.gca().xaxis.set_major_formatter(myFmt)
+    plt.setp(ax.get_xticklabels(), rotation=30, ha="center")
 
     plt.show()
-    plt.close()
 
 
 if __name__ == "__main__":
