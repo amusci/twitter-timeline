162d34718b5d998b417a8f56fdbfcb2b36940236
Antonio Musciano <antoniomusciano@gmail.com>
2021-08-06 23:45:06 -0400

    stripping the date =)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
+import matplotlib
+import matplotlib.pyplot as plt
 
 import tweepy
+
 from keys import keys
 
 CONSUMER_KEY = keys['consumer_key']
@@ -8,7 +10,7 @@ CONSUMER_SECRET = keys['consumer_secret']
 ACCESS_TOKEN = keys['access_token']
 ACCESS_TOKEN_SECRET = keys['access_token_secret']
 AT = keys['handle']
-
+y = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -22,15 +24,25 @@ except:
 
 
 def gathering():
-    tweetids = []
+    plt.style.use('seaborn')
+    list_of_datetimes = []
     for status in api.user_timeline(AT):
-        # print(status.created_at)
-        tweetids.append(str(status.created_at))  # time is in UTC
-    print(tweetids)
+
+        list_of_datetimes.append(str(status.created_at))  # time is in UTC
+
+    for dates in list_of_datetimes:
+        x = dates.split()[-1]
+        print(x)
+
+
+
+    # x = matplotlib.dates.date2num(list_of_datetimes)
+    # plt.plot_date(x, y)
+    # plt.tight_layout()
+    # plt.show()
 
 
 if __name__ == "__main__":
 
-    while True:
-        gathering()
 
+        gathering()
