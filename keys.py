177bc85e4f656d605c0511b0767ceb1ea5f790db
Antonio Musciano <71357098+antoniomusciano@users.noreply.github.com>
2021-08-11 12:10:37 -0400

    Delete keys.py

--- keys.py
+++
@@ -1,8 +0,0 @@
-keys = dict(
-    consumer_key='Z16m84auwjtJRmoIGO2xkySh2',
-    consumer_secret='N751lvtWm3FB3DcSygRfPDU1q6mSnlsINFHr6fFRIcsgCwiCKM',
-    access_token='1185419976976543744-G7kQ0RxBdt2doOeG1DDAJGa5w7DGbb',
-    access_token_secret='WJSvZnrDmw7AUU3KSsf8ydLefgEe3riHWjT2qTbh9WeG7',
-    handle='winstonwhirloo2',
-
-)
